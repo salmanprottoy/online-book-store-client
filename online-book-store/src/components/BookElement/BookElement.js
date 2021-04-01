bfbf0ccc6278970be4873b61ebd55a4d1f061c90
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-04-01 20:57:40 +0600

    Update BookElement.js

--- online-book-store/src/components/BookElement/BookElement.js
+++ online-book-store/src/components/BookElement/BookElement.js
@@ -1,18 +1,36 @@
 import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const BookElement = ({ book }) => {
+  const history = useHistory();
+  const deleteBook = (id) => {
+    console.log(id);
+    fetch(`http://localhost:5000/delete/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result) {
+          alert("Order confirmed");
+          history.push("/manageBook");
+        }
+      });
+  };
   return (
     <tr>
       <td>{book.bookName}</td>
       <td>{book.authorName}</td>
       <td>${book.bookPrice}</td>
       <td>
-        <button className="btn btn-success mr-1">
+        <button className="btn btn-success m-1">
           <FontAwesomeIcon className="m-auto" icon={faEdit} />
         </button>
-        <button className="btn btn-danger ml-1">
+        <button
+          onClick={() => deleteBook(book._id)}
+          className="btn btn-danger m-1"
+        >
           <FontAwesomeIcon className="m-auto" icon={faTrashAlt} />
         </button>
       </td>
